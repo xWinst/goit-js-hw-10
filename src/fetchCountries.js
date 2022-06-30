a3830b4e8c96cb9fae78d3c3d7cc55603a5f46c8
Oleg <hellreebok@gmail.com>
2022-06-30 17:53:29 +0300

    Bugs fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,9 @@
-export default function searchCountry(name) {
+export default function fetchCountries(value) {
+    const name = `https://restcountries.com/v3.1/name/${value}?fields=name,capital,population,flags,languages`;
     return fetch(name).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
-        response.json();
+        return response.json();
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import fetchCountry from './fetchCountries';
+import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -13,14 +13,13 @@ const DEBOUNCE_DELAY = 300;
 ref.input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
 
 function searchCountry(event) {
+    reset();
     let { value } = event.target;
     value = value.trim();
     if (!value) {
-        reset();
         return;
     }
-    const name = `https://restcountries.com/v3.1/name/${value}?fields=name,capital,population,flags,languages`;
-    fetchCountry(name).then(renderCountryData).catch(searchError);
+    fetchCountries(value).then(renderCountryData).catch(searchError);
 }
 
 function renderCountryData(countryData) {
@@ -31,46 +30,56 @@ function renderCountryData(countryData) {
         );
         return;
     }
-    let markup = '';
-    if (countryData.length > 1) {
-        countryData.forEach(({ name, flags }) => {
-            markup += `<li class="item">
-                           <img alt = "${name.common} flag" src = "${flags.svg}" width="50"> 
-                           <span>${name.common}</span>
-                       </li>`;
-        });
-        ref.countryList.innerHTML = markup;
-    } else {
-        const { name, flags, capital, population, languages } = countryData[0];
-        const langs = Object.values(languages);
-        markup = `<div class=thumb>
-                      <img alt = "${name.common} flag" 
-                          src = "${flags.svg}" 
-                          width="100"> 
-                      <span>${name.common}</span>
-                  </div>
-                  <p class="prop">Official name:  
-                      <span>${name.official}</span>
-                  </p>
-                  <p class="prop">Capital:  
-                      <span>${capital}</span>
-                  </p>
-                  <p class="prop">Population:  
-                      <span>${population}</span>
-                  </p>
-                  <p class="prop">Langueges:  
-                      <span>${langs.toString().replaceAll(',', ', ')}</span>
-                  </p>`;
 
-        ref.countryInfo.innerHTML = markup;
+    if (countryData.length > 1) {
+        ref.countryList.innerHTML = createList(countryData);
+    } else {
+        ref.countryInfo.innerHTML = createInfo(countryData[0]);
     }
 }
 
 function searchError(error) {
+    console.log(error);
     Notify.failure('Oops, there is no country with that name');
 }
 
 function reset() {
     ref.countryInfo.innerHTML = '';
     ref.countryList.innerHTML = '';
+}
+
+function createList(countries) {
+    const result = countries
+        .map(
+            ({ name, flags }) =>
+                `<li class="item">
+                <img alt = "${name.common} flag" src = "${flags.svg}" width="50"> 
+                <span>${name.common}</span>
+            </li>`
+        )
+        .join('');
+    return result;
+}
+
+function createInfo(country) {
+    const { name, flags, capital, population, languages } = country;
+    const langs = Object.values(languages);
+    return `<div class=thumb>
+                <img alt = "${name.common} flag" 
+                    src = "${flags.svg}" 
+                    width="100"> 
+                <span>${name.common}</span>
+            </div>
+            <p class="prop">Official name:  
+                <span>${name.official}</span>
+            </p>
+            <p class="prop">Capital:  
+                <span>${capital}</span>
+            </p>
+            <p class="prop">Population:  
+                <span>${population}</span>
+            </p>
+            <p class="prop">Langueges:  
+                <span>${langs.toString().replaceAll(',', ', ')}</span>
+            </p>`;
 }
